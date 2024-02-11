f171094d8842fd627d71eda4e7640640fb2648b6
KO <obrienk@webbhost.net>
2024-02-10 23:00:35 -0600

    hotfix z

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '', // Usually your GitHub org/user name.
+  organizationName: 'obrien-k', // Usually your GitHub org/user name.
   projectName: 'korin-omnibus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
